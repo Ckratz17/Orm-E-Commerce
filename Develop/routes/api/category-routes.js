0f7380992d3ffe452897aeac33d82b6fe28e00c0
Chris Kratz <Ckratz17@gmail.com>
2022-07-30 01:35:58 -0400

    fixed the env file

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -37,7 +37,9 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const catData = await Category.create(req.body.category_name)
+    const catData = await Category.create({
+      category_name: req.body.category_name
+    })
     res.status(200).json(catData)
   }catch (err) {
     res.status(400).json(err)
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -35,7 +35,11 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.create(req.body.tag_name)
+    const tagData = await Tag.create({
+      where: {
+        tag_name: req.body.tag_name
+      }
+    })
     res.status(200).json(tagData)
   }catch (err) {
     res.status(400).json(err)
